4f4db5df02b398459e2c060852dc62bc3f766026
Mark Seemann <mark@ploeh.dk>
2023-08-08 13:49:55 +0200

    Add StringParser

---
+++ StringParser.cs
@@ -0,0 +1,29 @@
+﻿namespace Ploeh.Katas.ArgsCSharp
+{
+    internal class StringParser
+    {
+        private char flagName;
+
+        public StringParser(char flagName)
+        {
+            this.flagName = flagName;
+        }
+
+        internal Validated<string, string> TryParse(string candidate)
+        {
+            var idx = candidate.IndexOf($"-{flagName}");
+
+            var nextFlagIdx = candidate[(idx + 2)..].IndexOf('-');
+            var sFlag = FindValue(candidate, idx, nextFlagIdx);
+            return Validated.Succeed<string, string>(sFlag);
+        }
+
+        private static string FindValue(string candidate, int idx, int nextFlagIdx)
+        {
+            if (nextFlagIdx < 0)
+                return candidate[(idx + 2)..].Trim();
+
+            return candidate.Substring(idx + 2, nextFlagIdx).Trim();
+        }
+    }
+}
---
+++ StringParserTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Ploeh.Katas.ArgsCSharp;
+
+public sealed class StringParserTests
+{
+    [Theory]
+    [InlineData('g', "-g foo", "foo")]
+    [InlineData('d', "-l -p 8080 -d /usr/logs", "/usr/logs")]
+    [InlineData('d', "-p 8080 -d /usr/logs -l", "/usr/logs")]
+    public void TryParseSuccess(char flagName, string candidate, string expected)
+    {
+        var sut = new StringParser(flagName);
+        var actual = sut.TryParse(candidate);
+        Assert.Equal(Validated.Succeed<string, string>(expected), actual);
+    }
+}
