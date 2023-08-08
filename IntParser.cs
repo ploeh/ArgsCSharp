91747bc26b203576895cf77e1ad064bc2cbbab90
Mark Seemann <mark@ploeh.dk>
2023-08-08 13:36:42 +0200

    Extract FindValue helper function
    I wasn't happy with trimming `iFlag` only when producing the error
    message. I'm not entirely happy with this implementation, either, since
    I find the duplication of `Trim()` offensive, but I'll invoke the Rule
    of Three and move on.

--- IntParser.cs
+++ IntParser.cs
@@ -17,13 +17,19 @@ public sealed class IntParser : IParser<int>
                 $"Missing value for flag '-{flagName}'.");
 
         var nextFlagIdx = candidate[(idx + 2)..].IndexOf('-');
-        var iFlag = nextFlagIdx < 0
-            ? candidate[(idx + 2)..]
-            : candidate.Substring(idx + 2, nextFlagIdx);
+        string iFlag = FindValue(candidate, idx, nextFlagIdx);
         if (int.TryParse(iFlag, out var i))
             return Validated.Succeed<string, int>(i);
 
         return Validated.Fail<string, int>(
-            $"""Expected integer for flag '-{flagName}', but got "{iFlag.Trim()}".""");
+            $"""Expected integer for flag '-{flagName}', but got "{iFlag}".""");
+    }
+
+    private static string FindValue(string candidate, int idx, int nextFlagIdx)
+    {
+        if (nextFlagIdx < 0)
+            return candidate[(idx + 2)..].Trim();
+
+        return candidate.Substring(idx + 2, nextFlagIdx).Trim();
     }
 }
