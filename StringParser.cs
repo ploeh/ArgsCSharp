8acbbef82e6e5606caa669ae9faac8132d65474a
Mark Seemann <mark@ploeh.dk>
2023-08-08 13:54:10 +0200

    Check for missing string flag

--- StringParser.cs
+++ StringParser.cs
@@ -1,29 +1,31 @@
-﻿namespace Ploeh.Katas.ArgsCSharp
+﻿namespace Ploeh.Katas.ArgsCSharp;
+
+public sealed class StringParser : IParser<string>
 {
-    internal class StringParser
+    private readonly char flagName;
+
+    public StringParser(char flagName)
     {
-        private char flagName;
+        this.flagName = flagName;
+    }
 
-        public StringParser(char flagName)
-        {
-            this.flagName = flagName;
-        }
+    public Validated<string, string> TryParse(string candidate)
+    {
+        var idx = candidate.IndexOf($"-{flagName}");
+        if (idx < 0)
+            return Validated.Fail<string, string>(
+                $"Missing value for flag '-{flagName}'.");
 
-        internal Validated<string, string> TryParse(string candidate)
-        {
-            var idx = candidate.IndexOf($"-{flagName}");
+        var nextFlagIdx = candidate[(idx + 2)..].IndexOf('-');
+        var sFlag = FindValue(candidate, idx, nextFlagIdx);
+        return Validated.Succeed<string, string>(sFlag);
+    }
 
-            var nextFlagIdx = candidate[(idx + 2)..].IndexOf('-');
-            var sFlag = FindValue(candidate, idx, nextFlagIdx);
-            return Validated.Succeed<string, string>(sFlag);
-        }
+    private static string FindValue(string candidate, int idx, int nextFlagIdx)
+    {
+        if (nextFlagIdx < 0)
+            return candidate[(idx + 2)..].Trim();
 
-        private static string FindValue(string candidate, int idx, int nextFlagIdx)
-        {
-            if (nextFlagIdx < 0)
-                return candidate[(idx + 2)..].Trim();
-
-            return candidate.Substring(idx + 2, nextFlagIdx).Trim();
-        }
+        return candidate.Substring(idx + 2, nextFlagIdx).Trim();
     }
 }
--- StringParserTests.cs
+++ StringParserTests.cs
@@ -8,10 +8,24 @@ public sealed class StringParserTests
     [InlineData('g', "-g foo", "foo")]
     [InlineData('d', "-l -p 8080 -d /usr/logs", "/usr/logs")]
     [InlineData('d', "-p 8080 -d /usr/logs -l", "/usr/logs")]
+    [InlineData('n', "-n fadsa -m gury", "fadsa")]
     public void TryParseSuccess(char flagName, string candidate, string expected)
     {
         var sut = new StringParser(flagName);
         var actual = sut.TryParse(candidate);
         Assert.Equal(Validated.Succeed<string, string>(expected), actual);
+    }
+
+    [Theory]
+    [InlineData('t', "")]
+    [InlineData('q', "")]
+    [InlineData('f', "-l -p 8080 -d /usr/logs")]
+    public void TryParseMissing(char flagName, string candidate)
+    {
+        var sut = new StringParser(flagName);
+        var actual = sut.TryParse(candidate);
+        Assert.Equal(
+            Validated.Fail<string, string>($"Missing value for flag '-{flagName}'."),
+            actual);
     }
 }
